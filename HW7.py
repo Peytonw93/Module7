1228370e4c98c3e2ad2dac644125cc894db22eb1
Peyton Wiederspan <jonwiederspan@BasicallyPros-MacBook-Pro.local>
2017-02-26 19:32:01 -0800

    Finalized Homework 7

--- HW7.py
+++ HW7.py
@@ -1,4 +1,3 @@
-from collections import OrderedDict
 from sortedcontainers import SortedDict
 #
 #Use this to show options
@@ -6,7 +5,7 @@ def print_menu():
     print('1. Print Users')
     print('2. Add a User')
     print('3. Remove a User')
-    print('4. Lookup a Phone Number')
+    print('4. Lookup a UserName')
     print('5. Quit')
     print()
 
@@ -27,8 +26,14 @@ print_menu()
 # as long as the menu choice isn't "quit" get user options
 while menu_choice != 5:
     # get menu choice from user
-    menu_choice = int(input("Type in a number (1-5): "))
-    
+    try:
+        # Have the user type one of the options
+        menu_choice = int(input("Type in a number (1-5): "))
+    # If the user types something other than an integer stop and go back to the menu
+    except:
+        print("Sorry, we're gonna need an Integer.\n\n\n")
+        
+
     # view current entries
     if menu_choice == 1:
         print("Current Users:")
@@ -40,23 +45,30 @@ while menu_choice != 5:
         print("Add User")
         name = input("Name: ")
         username = input("User Name: ")
-        usernames[name] = username
-        
+        if name == "":
+            print("Please enter something!\n\n\n")
+        else:
+            usernames[name] = username
+
     # remove an entry
     elif menu_choice == 3:
         print("Remove User")
         name = input("Name: ")
         if name in usernames:
-            pass # delete that entry
+            del usernames[name]
+        else:
+            # Tell the user if a name is in the system
+            print("Sorry dude, can't delete what's not there!\n\n\n") 
+
 
     # view user name      
     elif menu_choice == 4:
         print("Lookup User")
         name = input("Name: ")
         if name in usernames:
-            pass # print the username
+            print(usernames[name])
         else:
-            pass # print username not found
+            print("Whoops, that name is not in our system!\n\n\n")
     
     # is user enters something strange, show them the menu
     elif menu_choice != 5:
